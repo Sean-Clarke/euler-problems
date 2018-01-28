a3c168996d6277e41cf669ca387bf4e39cff1276
Sean Clarke <sean.alexander.clarke@gmail.com>
2018-01-27 19:08:09 -0800

    completed p9

--- p9.py
+++ p9.py
@@ -1,8 +1,16 @@
 import math
 
-abc_sum = 1000
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a2 + b2 = c2
+#
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
 
-# a ** 2 + b **2 = c ** 2
-
-triplet = [1, 2, 3]
-
+for a in range(1, 333):
+    for b in range(a, 500):
+        if (a**2 + b**2 == (1000 - a - b)**2):
+            print(a*b*(1000 - a - b))
+            break
