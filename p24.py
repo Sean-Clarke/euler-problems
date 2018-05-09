f8cf7af63763a4051f866e2e6e55196743a330ad
Sean Clarke <sean.alexander.clarke@gmail.com>
2018-05-09 00:15:00 -0700

    Completed p24

--- p24.py
+++ p24.py
@@ -9,6 +9,8 @@
 
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+import math
+
 def make_p(_digits):
     _p = 1
     p_count = 2
@@ -19,7 +21,18 @@ def make_p(_digits):
 
     return _p
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-_p = make_p(len(numbers))
-p = 1000000
+def lex_perm_of(p, _d):
+    _p = make_p(len(_d))
+    answer = []
 
+    while len(_d) > 0:
+        f = _p / len(_d)
+        i = math.ceil(p * len(_d) / _p) - 1
+        answer.append(_d.pop(i))
+        _p = f
+        f *= i
+        p -= f
+
+    return answer
+
+print(lex_perm_of(1000000, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
